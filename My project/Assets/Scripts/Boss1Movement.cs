f00a0a9325e2603962c493b216c88bc1103d0c5d
Platinem <joonaj8@gmail.com>
2023-03-24 12:17:21 +0200

    latest2
    dsa2

---
+++ My project/Assets/Scripts/Boss1Movement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Boss1Movement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private int waypoint = 0;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        Move();
+    }
+    
+    // Method that makes the enemy move
+    private void Move()
+    {
+
+        // If enemy reached the last waypoint then it stops
+        if (waypoint <= waypoints.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            transform.position = Vector2.MoveTowards(transform.position,
+               waypoints[waypoint].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // When enemy moves to the next waypoint, add 1 to the waypoint index
+            if (transform.position.x == waypoints[waypoint].transform.position.x &&
+                transform.position.y == waypoints[waypoint].transform.position.y)
+            {
+                waypoint += 1;
+            }
+        }
+    }
+}
